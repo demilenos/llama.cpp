07c232edfd06b4bc26d901720548ffbf99a4842d
demilenos <demilenos@users.noreply.github.com>
2026-09-18 22:25:07 +0900

    show -fitp

--- common/arg.cpp
+++ common/arg.cpp
@@ -2874,7 +2874,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
                     string_format("error: unknown value for --fit-print: '%s'\n", value.c_str()));
             }
         }
-    ).set_examples({LLAMA_EXAMPLE_FIT_PARAMS}).set_env("LLAMA_ARG_FIT_ESTIMATE"));
+    ).set_examples({LLAMA_EXAMPLE_FIT_PARAMS, LLAMA_EXAMPLE_SERVER}).set_env("LLAMA_ARG_FIT_ESTIMATE"));
     add_opt(common_arg(
         { "-fitt", "--fit-target" }, "MiB0,MiB1,MiB2,...",
         string_format("target margin per device for --fit, comma-separated list of values, "
